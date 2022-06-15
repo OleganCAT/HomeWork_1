af42f0b9c4c05c213c1bb14f0088233617d4cec3
an40us <oleganfoya@gmail.com>
2022-06-15 14:51:48 +0200

    two exercise ready

--- main.go
+++ main.go
@@ -5,13 +5,17 @@ import (
 )
 
 var (
-	PriceApple  float64 = 5.99
-	PricePear   int     = 7
-	AmountMoney int     = 23
+	priceApple  float64 = 5.99
+	pricePear   int     = 7
+	amountMoney int     = 23
 )
 
 func main() {
 
-	var purchases float64 = (PriceApple * float64(9)) + (float64(PricePear) * float64(8))
-	fmt.Println("Скільки грошей треба витратити, щоб купити 9 яблук та 8 груш? :", "Have to spand -", purchases, "UAH")
+	appelsAndPears := (priceApple * float64(9)) + (float64(pricePear) * float64(8))
+	fmt.Println("1. Скільки грошей треба витратити, щоб купити 9 яблук та 8 груш? :", "Have to spand -", appelsAndPears, "UAH")
+
+	numberOfPears := amountMoney / pricePear
+	fmt.Println("2. Скільки груш ми можемо купити? :", "We can buy -", numberOfPears, "Pears")
+
 }
