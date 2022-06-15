cd238c52d00f41c1f4c5f9ae8ba618eed8e6e676
an40us <oleganfoya@gmail.com>
2022-06-15 14:13:04 +0200

    one execise ready

--- main.go
+++ main.go
@@ -1,4 +1,17 @@
-clear
-cd
-quit
-exit
+package main
+
+import (
+	"fmt"
+)
+
+var (
+	PriceApple  float64 = 5.99
+	PricePear   int     = 7
+	AmountMoney int     = 23
+)
+
+func main() {
+
+	var purchases float64 = (PriceApple * float64(9)) + (float64(PricePear) * float64(8))
+	fmt.Println("Скільки грошей треба витратити, щоб купити 9 яблук та 8 груш? :", "Have to spand -", purchases, "UAH")
+}
