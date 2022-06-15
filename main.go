73f8aeb92a6c3aa9da8e7d1cdd9de234e374d088
an40us <oleganfoya@gmail.com>
2022-06-15 15:08:33 +0200

    three exercise ready

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-var (
+const (
 	priceApple  float64 = 5.99
 	pricePear   int     = 7
 	amountMoney int     = 23
@@ -15,7 +15,10 @@ func main() {
 	appelsAndPears := (priceApple * float64(9)) + (float64(pricePear) * float64(8))
 	fmt.Println("1. Скільки грошей треба витратити, щоб купити 9 яблук та 8 груш? :", "Have to spand -", appelsAndPears, "UAH")
 
-	numberOfPears := amountMoney / pricePear
-	fmt.Println("2. Скільки груш ми можемо купити? :", "We can buy -", numberOfPears, "Pears")
+	amountOfPears := amountMoney / pricePear
+	fmt.Println("2. Скільки груш ми можемо купити? :", "We can buy -", amountOfPears, "Pears")
+
+	amountOfApples := float64(amountMoney) / priceApple
+	fmt.Println("3. Скільки яблук ми можемо купити? :", "We can buy -", amountOfApples, "Apples")
 
 }
